aaf31f8bcc7d46dab9d02fdc8297e83c13915001
LoganC <alclary@posteo.net>
2023-02-16 22:02:08 -0600

    styling and selector for schemaGraph

--- frontend/src/components/Schema.js
+++ frontend/src/components/Schema.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Schema() {
   return (
-    <div className="">
+    <div className="" id="schemaGraph">
 <h3>Schema</h3>
 <img src="../images/schema.png" alt="Schema"></img>
 
