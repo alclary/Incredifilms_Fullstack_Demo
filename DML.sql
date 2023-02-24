37609108b77b4fc6a7e567bdeedc091aa5b5a7e6
LoganC <alclary@posteo.net>
2023-02-23 20:46:22 -0600

    DML fixes; extra DMLs in separate file

--- DML.sql
+++ DML.sql
@@ -1,79 +1,57 @@
-/* 
-CS 340 - Project Step 3 
-Data Manipulation Queries (DML) 
+/*
+CS 340 - Project Step 3
+Data Manipulation Queries (DML)
 Group #25
 Jesseline Velazquez, Anthony Logan Clary
 */
 
-
--- SIMPLE LISTS ------------------------------------------------------------------------
+-- DROP-DOWN LIST QUERIES ------------------------------------------------------------------------
 -- Get list of all genres
 SELECT genre_name FROM Genre;
 
--- Get list of all movies
-SELECT movie_name FROM Movie;
+-- Get list of all movies in format [movie_name (movie_id)] and associated movie_id
+-- To be used to populate dropdown list and map selection to movie_id
+SELECT CONCAT(movie_name, ' (', movie_id, ')') AS movie, movie_id FROM Movie;
 
 -- Get list of all theaters
 SELECT theater_name FROM Theater;
 
--- Get list of all customers by full name
-SELECT CONCAT(Customer.first_name, ' ', Customer.last_name) 
-AS "Customer Name"
-FROM Customer; 
-
--- Get list of all tickets
-SELECT * FROM Ticket;
-
--- Get list of all showtimes
-SELECT * FROM Showtime;
-
--- Get list of all Movie_Genre intersection table
-SELECT * FROM Movie_Genre;
-
-
--- SELECTS -----------------------------------------------------------------------------
 -- Get list of customer names [last, first, (customer_id)] and associated customer_id
-SELECT CONCAT(last_name, ', ', first_name, ' (', customer_id, ')') AS customer, 
-	customer_id 
+-- To be used to populate dropdown list and map selection to customer_id
+SELECT CONCAT(last_name, ', ', first_name, ' (', customer_id, ')') AS customer,
+	customer_id
 FROM Customer ORDER BY Customer.last_name;
 
--- Get list of all genres associated with a given movie
-SELECT Genre.genre_name AS Genre
-FROM Movie_Genre
-JOIN Genre ON Movie_Genre.genre_id = Genre.genre_id
-JOIN Movie ON Movie_Genre.movie_id = Movie.movie_id 
-    AND Movie.movie_name = :movieNameFromDropdownList
-ORDER BY Genre;
+-- SELECTS -----------------------------------------------------------------------------
+-- Get full Movie table
+SELECT movie_id AS 'Movie ID', movie_name AS "Movie Name", runtime_min AS 'Runetime (mins)', mpa_rating AS 'MPA Rating', movie_year AS 'Year'
+FROM Movie;
 
--- Get list of all showtimes for a given movie
-SELECT Showtime.showtime_date_time AS Showtime
-FROM Movie
-JOIN Showtime ON Movie.movie_id = Showtime.movie_id
-WHERE Movie.movie_name = :movieNameFromDropdownList
-ORDER BY Showtime;
+-- Get full Genre table
+SELECT genre_id AS 'Genre ID', genre_name AS 'Genre Name'
+FROM Genre;
 
--- Get list of all showtimes for a given movie between two given dates
-SELECT Showtime.showtime_date_time AS Showtime
-FROM Movie
-JOIN Showtime ON Movie.movie_id = Showtime.movie_id
-WHERE Movie.movie_name = :movieNameFromDropdownList
-AND Showtime BETWEEN :dateTimeInput1 AND :dateTimeInput2
-ORDER BY Showtime;
+-- Get full Theater table
+SELECT theater_id AS 'Theater ID', theater_name AS 'Theater Name', no_of_seats AS 'Number of seats'
+FROM Theater;
 
--- Get list of all showtimes for a given theater
-SELECT Showtime.showtime_date_time AS Showtime
-FROM Theater
-JOIN Showtime ON Theater.theater_id = Showtime.theater_id
-WHERE Theater.theater_name = :theaterNameFromDropdownList
-ORDER BY Showtime;
+-- Get full Customer table
+SELECT customer_id AS 'Customer ID', first_name AS 'First Name', last_name AS 'Last Name', dob AS 'Date of Birth', email AS 'Email'
+FROM Customer;
 
--- Get list of all showtimes for a given theater between two given dates
-SELECT Showtime.showtime_date_time AS Showtime
-FROM Theater
-JOIN Showtime ON Theater.theater_id = Showtime.theater_id
-WHERE Theater.theater_name = :theaterNameFromDropdownList
-AND Showtime BETWEEN :dateTimeInput1 AND :dateTimeInput2
-ORDER BY Showtime;
+-- Get full Showtime table, with readable movie & theater names
+SELECT Showtime.showtime_id AS 'Showtime ID', Showtime.showtime_date_time AS 'Datetime', Movie.movie_name AS 'Movie', Theater.theater_name AS 'Theater'
+FROM Showtime
+JOIN Movie ON Showtime.movie_id = Movie.movie_id
+JOIN Theater ON Showtime.theater_id = Theater.theater_id;
+
+-- Get full Ticket table, with readable customer, movie, theater
+SELECT Ticket.ticket_id AS 'Ticket ID', CONCAT(Customer.first_name, ' ', Customer.last_name) AS Customer, Showtime.showtime_date_time AS 'Showtime Datetime', Movie.movie_name AS Movie, Theater.theater_name AS Theater, Ticket.price AS Price, Ticket.payment_method AS 'Payment Method'
+FROM Ticket
+JOIN Customer ON Ticket.customer_id = Customer.customer_id
+JOIN Showtime ON Ticket.showtime_id = Showtime.showtime_id
+JOIN Movie ON Showtime.movie_id = Movie.movie_id
+JOIN Theater ON Showtime.theater_id = Theater.theater_id;
 
 -- Get list of all movies with showtimes between two given dates
 SELECT Movie.movie_name AS Movie
@@ -82,66 +60,22 @@ JOIN Movie ON Showtime.movie_id = Movie.movie_id
 WHERE Showtime.showtime_date_time BETWEEN :dateTimeInput1 AND :dateTimeInput2
 GROUP BY Movie;
 
--- Get list of all theaters with showtimes between two given dates
-SELECT Theater.theater_name AS Theater
-FROM Showtime
-JOIN Theater ON Showtime.theater_id = Theater.theater_id
-WHERE Showtime.showtime_date_time BETWEEN :dateTimeInput1 AND :dateTimeInput2
-GROUP BY Theater;
-
--- Get list of all showtime (concat format) across theaters
-SELECT showtime_id,
-    Theater.theater_name AS Theater, 
-    CONCAT(Movie.movie_name, " ", Showtime.showtime_date_time) AS Showtime
-FROM Showtime
-JOIN Theater ON Showtime.theater_id = Theater.theater_id
-JOIN Movie ON Showtime.movie_id = Movie.movie_id
-ORDER BY Theater, Showtime.showtime_date_time;
-
--- Get list of all movies appropriate for Kids (PG rating and/or "Kids" Genre)
-
-SELECT DISTINCT Movie.movie_name, Movie.runtime_min, Movie.mpa_rating, Movie.movie_year
-FROM Movie
-
-JOIN Movie_Genre
-ON Movie.movie_id = Movie_Genre.movie_genre_id
-
-AND Movie.movie_id
-
-IN (
-	SELECT Movie.movie_id
-    FROM Movie
-    
-    JOIN Movie_Genre
-    ON Movie_Genre.movie_id = Movie.movie_id
-    
-    JOIN Genre
-    ON Genre.genre_id = Movie_Genre.genre_id
-    AND (Genre.genre_name = "Kids" 
-    OR Genre.genre_name = "Family")
-)
-
-OR Movie.mpa_rating = "PG"
-
-ORDER BY Movie.movie_name;
-
-
 -- INSERTS -----------------------------------------------------------------------------
--- Add a customer
-INSERT INTO Customer (first_name, last_name, dob, email)
-VALUES (:fnameInput, :lnameInput, :dobInput, :emailInput);
-
--- Add a genre
-INSERT INTO Genre (genre_name) 
-VALUES (:genreInput);
-
 -- Add a movie
 INSERT INTO Movie (movie_name, runtime_min, mpa_rating, movie_year)
 VALUES (:movieNameInput, :runtimeMinInput, :mpaRatingInput, :movieYearInput);
 
+-- Add a genre
+INSERT INTO Genre (genre_name)
+VALUES (:genreInput);
+
 -- Add a theater
-INSERT INTO Theater (theater_name, no_of_seats) 
+INSERT INTO Theater (theater_name, no_of_seats)
 VALUES (:theaterNameInput, :noOfSeatsInput);
+
+-- Add a customer
+INSERT INTO Customer (first_name, last_name, dob, email)
+VALUES (:fnameInput, :lnameInput, :dobInput, :emailInput);
 
 -- Associate a genre with a movie
 INSERT INTO Movie_Genre (movie_id, genre_id)
@@ -167,38 +101,43 @@ VALUES (
     :paymentMethodFromHardCodedList
 );
 
-
 -- UPDATES -----------------------------------------------------------------------------
 /* NOTE: Movie_Genre table is not permitted to be updated directly (cascade or delete+recreate);
 Tickets table is not permitted to be updated directly (cascade or delete+recreate).*/
-
--- Update a customer
-UPDATE Customer
-SET first_name = :fnameInput, last_name = :lnameInput, dob = :dobInput, email = :emailInput
-WHERE customer_id = :customerIdFromListOrControlledInput
-
--- Update a genre (i.e. mispelling)
-UPDATE Genre
-SET genre_name = :genreNameInput
-WHERE genre_id = :genreIdFromListOrControlledInput
 
 -- Update a movie
 UPDATE Movie
 SET movie_name = :movieNameInput, runtime_min = :runtimeMinInput, mpa_rating = :ratingFromHardcodedList, movie_year = :movieYearInput
 WHERE movie_id = :movieIdFromListOrControlledInput
 
+-- Update a genre (i.e. mispelling)
+UPDATE Genre
+SET genre_name = :genreNameInput
+WHERE genre_id = :genreIdFromListOrControlledInput
+
 -- Update a theater
 UPDATE Theater
 SET theater_name = :theaterNameInput, no_of_seats = :noOfSeatsInput
 WHERE theater_id = :theaterIdFromListOrControlledInput
 
+-- Update a customer
+UPDATE Customer
+SET first_name = :fnameInput, last_name = :lnameInput, dob = :dobInput, email = :emailInput
+WHERE customer_id = :customerIdFromListOrControlledInput
+
 -- Update a ticket
 UPDATE Ticket
-SET 
+SET customer_id = :customerIdfromDropDownListOfUniqueCustomers,
+    showtime_id = :showtimeIdfromDropDownListOfUniqueShowtimes,
+    price = :priceInput,
+    payment_method = :paymentMethodFromHardCodedList
+WHERE ticket_id = :ticketIdFromListOrControlledInput
 
--- Update a showtime date/time ONLY
+-- Update a showtime
 UPDATE Showtime
-SET showtime_date_time = :dateTimeInput
+SET showtime_date_time = :dateTimeInput,
+    movie_id = :movieNameFromDropdownList,
+    theater_id = :theaterNameFromDropdownList
 WHERE showtime_id = :showtimeIdFromListOrControlledInput
 
 -- DELETES -----------------------------------------------------------------------------
@@ -222,70 +161,5 @@ DELETE FROM Showtime WHERE showtime_id = :showtimeIdFromListOrControlledInput
 -- Delete a ticket
 DELETE FROM Ticket WHERE ticket_id = :ticketIdFromListOrControlledInput
 
-
--- JOINS to make FK user friendly ---------------------------------------
-
--- For Ticket UI: Display Ticket ID, Customer name (first + last name), Movie Name, Theater Name, Price, Payment Method
-
-SELECT Ticket.ticket_id as "Ticket ID", 
-CONCAT(Customer.first_name, ' ', Customer.last_name) as "Customer Name",
-Movie.movie_name as "Movie Name", 
-Theater.theater_name as "Theater Name", 
-Ticket.price as "Price", 
-Ticket.payment_method as "Payment Method"
-FROM Ticket
-INNER JOIN Customer
-ON Customer.customer_id = Ticket.customer_id
-INNER JOIN Showtime
-ON Ticket.showtime_id = Showtime.showtime_id
-INNER JOIN Movie
-ON Movie.movie_id = Showtime.movie_id
-INNER JOIN Theater
-ON Theater.theater_id = Showtime.showtime_id
-ORDER BY Ticket.ticket_id;
-
--- For Movie_Genre UI: Display Movie Name, Genre Name
-
-SELECT Movie_Genre.movie_genre_id AS "Movie Genre ID",
-Movie.movie_name AS "Movie Name",
-Genre.genre_name AS "Genre Name"
-FROM Movie_Genre
-INNER JOIN Movie
-ON Movie.movie_id = Movie_Genre.movie_id
-INNER JOIN Genre
-ON Genre.genre_id = Movie_Genre.genre_id
-ORDER BY Movie.movie_name;
-
---  For Showtime UI: Display Showtime ID, Showtime Date & Time, Movie Name, Theater Name
-
-SELECT Showtime.showtime_id as "Showtime ID", Showtime.showtime_date_time as "Showtime Date & Time", Movie.movie_name as "Movie Name", Theater.theater_name as "Theater Name"
-FROM Showtime
-JOIN Movie
-ON Movie.movie_id = Showtime.showtime_id
-JOIN Theater
-on Theater.theater_id = Showtime.theater_id
-ORDER BY showtime_date_time ASC;
-
--- For Ticket UI: Display Ticket ID, Customer Name, Movie Name, Theater Name, Price, Payment Method
-
-SELECT Ticket.ticket_id AS "Ticket ID", 
-CONCAT(Customer.first_name, ' ', Customer.last_name) as "Customer Name", Showtime.showtime_date_time AS "Showtime Date & Time", 
-Movie.movie_name as "Movie Name", 
-Theater.theater_name as "Theater Name", 
-Ticket.price as "Price", Ticket.payment_method as "Payment Method"
-
-FROM Ticket
-
-JOIN Customer
-ON Customer.customer_id = Ticket.customer_id
-
-JOIN Showtime
-ON Showtime.showtime_id = Ticket.showtime_id
-
-JOIN Movie
-ON Movie.movie_id = Showtime.movie_id
-
-JOIN Theater 
-ON Theater.theater_id = Showtime.theater_id
-
-ORDER BY Ticket.ticket_id;
+-- Delete a movie-genre association
+DELETE FROM Movie_genre WHERE movie_genre_id = :movieGenreIdFromListOrControlledInput
---
+++ DML_extra.sql
@@ -0,0 +1,175 @@
+/*
+CS 340 - Project Step 3
+Data Manipulation Extra Queries (DML)
+Group #25
+Jesseline Velazquez, Anthony Logan Clary
+*/
+
+-- DROP-DOWN LIST QUERIES ------------------------------------------------------------------------
+-- Get list of all customers by full name
+SELECT CONCAT(Customer.first_name, ' ', Customer.last_name)
+AS "Customer Name"
+FROM Customer;
+
+-- SELECTS -----------------------------------------------------------------------------
+-- Get list of all genres associated with a given movie
+SELECT Genre.genre_name AS Genre
+FROM Movie_Genre
+JOIN Genre ON Movie_Genre.genre_id = Genre.genre_id
+JOIN Movie ON Movie_Genre.movie_id = Movie.movie_id
+    AND Movie.movie_name = :movieNameFromDropdownList
+ORDER BY Genre;
+
+-- Get list of all showtimes for a given movie
+SELECT Showtime.showtime_date_time AS Showtime
+FROM Movie
+JOIN Showtime ON Movie.movie_id = Showtime.movie_id
+WHERE Movie.movie_name = :movieNameFromDropdownList
+ORDER BY Showtime;
+
+-- Get list of all showtimes for a given movie between two given dates
+SELECT Showtime.showtime_date_time AS Showtime
+FROM Movie
+JOIN Showtime ON Movie.movie_id = Showtime.movie_id
+WHERE Movie.movie_name = :movieNameFromDropdownList
+AND Showtime BETWEEN :dateTimeInput1 AND :dateTimeInput2
+ORDER BY Showtime;
+
+-- Get list of all showtimes for a given theater
+SELECT Showtime.showtime_date_time AS Showtime
+FROM Theater
+JOIN Showtime ON Theater.theater_id = Showtime.theater_id
+WHERE Theater.theater_name = :theaterNameFromDropdownList
+ORDER BY Showtime;
+
+-- Get list of all showtimes for a given theater between two given dates
+SELECT Showtime.showtime_date_time AS Showtime
+FROM Theater
+JOIN Showtime ON Theater.theater_id = Showtime.theater_id
+WHERE Theater.theater_name = :theaterNameFromDropdownList
+AND Showtime BETWEEN :dateTimeInput1 AND :dateTimeInput2
+ORDER BY Showtime;
+
+-- Get list of all movies with showtimes between two given dates
+SELECT Movie.movie_name AS Movie
+FROM Showtime
+JOIN Movie ON Showtime.movie_id = Movie.movie_id
+WHERE Showtime.showtime_date_time BETWEEN :dateTimeInput1 AND :dateTimeInput2
+GROUP BY Movie;
+
+-- Get list of all theaters with showtimes between two given dates
+SELECT Theater.theater_name AS Theater
+FROM Showtime
+JOIN Theater ON Showtime.theater_id = Theater.theater_id
+WHERE Showtime.showtime_date_time BETWEEN :dateTimeInput1 AND :dateTimeInput2
+GROUP BY Theater;
+
+-- Get list of all showtime (concat format) across theaters
+SELECT showtime_id,
+    Theater.theater_name AS Theater,
+    CONCAT(Movie.movie_name, " ", Showtime.showtime_date_time) AS Showtime
+FROM Showtime
+JOIN Theater ON Showtime.theater_id = Theater.theater_id
+JOIN Movie ON Showtime.movie_id = Movie.movie_id
+ORDER BY Theater, Showtime.showtime_date_time;
+
+-- Get list of all movies appropriate for Kids (PG rating and/or "Kids" Genre)
+
+SELECT DISTINCT Movie.movie_name, Movie.runtime_min, Movie.mpa_rating, Movie.movie_year
+FROM Movie
+
+JOIN Movie_Genre
+ON Movie.movie_id = Movie_Genre.movie_genre_id
+
+AND Movie.movie_id
+
+IN (
+	SELECT Movie.movie_id
+    FROM Movie
+
+    JOIN Movie_Genre
+    ON Movie_Genre.movie_id = Movie.movie_id
+
+    JOIN Genre
+    ON Genre.genre_id = Movie_Genre.genre_id
+    AND (Genre.genre_name = "Kids"
+    OR Genre.genre_name = "Family")
+)
+
+OR Movie.mpa_rating = "PG"
+
+ORDER BY Movie.movie_name;
+
+
+-- INSERTS -----------------------------------------------------------------------------
+
+-- UPDATES -----------------------------------------------------------------------------
+
+-- DELETES -----------------------------------------------------------------------------
+
+-- JOINS to make FK user friendly ---------------------------------------
+
+-- For Ticket UI: Display Ticket ID, Customer name (first + last name), Movie Name, Theater Name, Price, Payment Method
+
+SELECT Ticket.ticket_id as "Ticket ID",
+CONCAT(Customer.first_name, ' ', Customer.last_name) as "Customer Name",
+Movie.movie_name as "Movie Name",
+Theater.theater_name as "Theater Name",
+Ticket.price as "Price",
+Ticket.payment_method as "Payment Method"
+FROM Ticket
+INNER JOIN Customer
+ON Customer.customer_id = Ticket.customer_id
+INNER JOIN Showtime
+ON Ticket.showtime_id = Showtime.showtime_id
+INNER JOIN Movie
+ON Movie.movie_id = Showtime.movie_id
+INNER JOIN Theater
+ON Theater.theater_id = Showtime.showtime_id
+ORDER BY Ticket.ticket_id;
+
+-- For Movie_Genre UI: Display Movie Name, Genre Name
+
+SELECT Movie_Genre.movie_genre_id AS "Movie Genre ID",
+Movie.movie_name AS "Movie Name",
+Genre.genre_name AS "Genre Name"
+FROM Movie_Genre
+INNER JOIN Movie
+ON Movie.movie_id = Movie_Genre.movie_id
+INNER JOIN Genre
+ON Genre.genre_id = Movie_Genre.genre_id
+ORDER BY Movie.movie_name;
+
+--  For Showtime UI: Display Showtime ID, Showtime Date & Time, Movie Name, Theater Name
+
+SELECT Showtime.showtime_id as "Showtime ID", Showtime.showtime_date_time as "Showtime Date & Time", Movie.movie_name as "Movie Name", Theater.theater_name as "Theater Name"
+FROM Showtime
+JOIN Movie
+ON Movie.movie_id = Showtime.showtime_id
+JOIN Theater
+on Theater.theater_id = Showtime.theater_id
+ORDER BY showtime_date_time ASC;
+
+-- For Ticket UI: Display Ticket ID, Customer Name, Movie Name, Theater Name, Price, Payment Method
+
+SELECT Ticket.ticket_id AS "Ticket ID",
+CONCAT(Customer.first_name, ' ', Customer.last_name) as "Customer Name", Showtime.showtime_date_time AS "Showtime Date & Time",
+Movie.movie_name as "Movie Name",
+Theater.theater_name as "Theater Name",
+Ticket.price as "Price", Ticket.payment_method as "Payment Method"
+
+FROM Ticket
+
+JOIN Customer
+ON Customer.customer_id = Ticket.customer_id
+
+JOIN Showtime
+ON Showtime.showtime_id = Ticket.showtime_id
+
+JOIN Movie
+ON Movie.movie_id = Showtime.movie_id
+
+JOIN Theater
+ON Theater.theater_id = Showtime.theater_id
+
+ORDER BY Ticket.ticket_id;
