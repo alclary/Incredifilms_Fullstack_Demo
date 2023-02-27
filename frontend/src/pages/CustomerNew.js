a2e26bd6d02757c050a1542c9120ac7ed115427e
LoganC <alclary@posteo.net>
2023-02-26 23:57:59 -0600

    build out new customer form

--- frontend/src/pages/CustomerNew.js
+++ frontend/src/pages/CustomerNew.js
@@ -1,43 +1,56 @@
-import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export const CustomerNew = () => {
-  return (
-    <>
-      <article>
-        <h3>Add a new customer</h3>
-        <form className="form">
-          <label>
-            First name
-            <input type="text" required />
-          </label>
-          <br />
-          <label>
-            Last name
-            <input type="text" required />
-          </label>
-          <br />
-          <label>
-            Date of Birth
-            <input type="date" required />
-          </label>
-          <br />
+    const [fname, setFname] = useState("");
+    const [lname, setLname] = useState("");
+    const [dob, setDob] = useState("");
+    const [email, setEmail] = useState("");
 
-          <label>
-            Email
-            <input type="email" />
-          </label>
-          <br />
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // TODO implement POST request to API
+    };
 
-          <label>
-            <input type="submit" />
-          </label>
-        </form>
-      </article>
-
-      <a href="./Customer">Return to all customers</a>
-    </>
-  );
+    return (
+        <div>
+            <h3>Add a new customer</h3>
+            <form onSubmit={handleSubmit} class="pure-form pure-form-stacked">
+                <label for="stacked-fname">First name</label>
+                <input
+                    type="text"
+                    id="stacked-fname"
+                    required
+                    value={fname}
+                    onChange={(e) => setFname(e.target.value)}
+                />
+                <label for="stacked-lname">Last name</label>
+                <input
+                    type="text"
+                    required
+                    value={lname}
+                    onChange={(e) => setLname(e.target.value)}
+                />
+                <label for="stacked-dob">Date of Birth</label>
+                <input
+                    type="date"
+                    required
+                    value={dob}
+                    onChange={(e) => setDob(e.target.value)}
+                />
+                <label for="stacked-email">Email</label>
+                <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <button type="submit" class="pure-button pure-button-primary">
+                    Submit
+                </button>
+            </form>
+            <Link to="/customer">Return to all customers</Link>
+        </div>
+    );
 };
 
 export default CustomerNew;
