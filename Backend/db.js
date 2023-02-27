063f2f043b1227a61643aed4b8d669ff13a8e9e3
velazqje <velazqje@oregonstate.edu>
2023-02-27 14:29:46 -0500

    reset db.js connection to original

--- Backend/db.js
+++ Backend/db.js
@@ -2,18 +2,14 @@ require("dotenv").config();
 
 const mysql = require("mysql");
 const connection = mysql.createConnection({
-    // host: process.env.MYSQL_HOST,
-    // port: process.env.MYSQL_PORT,
-    // user: process.env.MYSQL_USER,
-    // password: process.env.MYSQL_PASS,
-    // database: process.env.DEFAULT_DB,
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASS,
+    database: process.env.DEFAULT_DB,
 
 
-    host: "classmysql.engr.oregonstate.edu",
-    user: "cs340_velazqje",
-    port: "3306",
-    password: "0424",
-    database: "cs340_velazqje",
+
   
 
 
