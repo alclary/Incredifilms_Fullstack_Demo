061de6c4dd7f43163a4bd1fb9f2224707f70fcea
LoganC <alclary@posteo.net>
2023-02-28 16:44:41 -0600

    remove whitespace

--- Backend/db.js
+++ Backend/db.js
@@ -7,13 +7,6 @@ const connection = mysql.createConnection({
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASS,
     database: process.env.DEFAULT_DB,
-
-
-
-  
-
-
-
 });
 
 connection.connect((err) => {
