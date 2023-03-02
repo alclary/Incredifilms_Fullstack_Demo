4ee002a484d364f5a40e18942a7f39345596f11c
LoganC <alclary@posteo.net>
2023-03-01 21:48:41 -0600

    live fetch customer table contents

--- frontend/src/pages/Customers.js
+++ frontend/src/pages/Customers.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Grid, _ } from "gridjs-react";
 import { MdEdit, MdDeleteForever } from "react-icons/md";
+import axios from "axios";
 
 export default function Customers() {
     return (
@@ -12,46 +13,12 @@ export default function Customers() {
                 Add new customer
             </Link>
             <Grid
-                data={[
-                    [1, "AJ", "Styles", "1977-06-02", "AJ.Styles@bmail.com"],
-                    [
-                        2,
-                        "Stephanie",
-                        "Helmsley",
-                        "1976-09-24",
-                        "Stephanie.Helmsely@bmail.com",
-                    ],
-                    [
-                        3,
-                        "Alexa",
-                        "Bliss",
-                        "1991-08-09",
-                        "Alexa.Bliss@bmail.com",
-                    ],
-                    [4, "Booker", "T", "1965-03-01", "NULL"],
-                    [
-                        5,
-                        "Jenna",
-                        "Andrade",
-                        "1989-11-03",
-                        "Jenna.Andrade@bmail.com",
-                    ],
-                    [6, "Andre", "Giant", "1946-05-19", "NULL"],
-                    [
-                        7,
-                        "Michaela",
-                        "Hargrove",
-                        "2001-05-30",
-                        "Michaela.Hargrove@bmail.com",
-                    ],
-                    [8, "Em", "Patterson", "2017-12-02", "NULL"],
-                ]}
                 columns={[
-                    { name: "Customer ID", sort: true },
-                    { name: "First Name", sort: true },
-                    { name: "Last Name", sort: true },
-                    "Date of Birth",
-                    "Email",
+                    { name: "Customer ID", id: "customer_id", sort: true },
+                    { name: "First Name", id: "first_name", sort: true },
+                    { name: "Last Name", id: "last_name", sort: true },
+                    { name: "Date of Birth", id: "dob" },
+                    { name: "Email", id: "email" },
                     {
                         name: "Edit Item",
                         data: _(<MdEdit onClick={() => alert("clicked!")} />),
@@ -65,6 +32,12 @@ export default function Customers() {
                         ),
                     },
                 ]}
+                data={async () => {
+                    const customers = await axios.get(
+                        "http://localhost:3001/customers"
+                    );
+                    return customers.data.data;
+                }}
                 search={true}
                 pagination={{ limit: 25 }}
             />
