ca03d11fb9ce804d04063ccae5fef47faa62bd23
velazqje <velazqje@oregonstate.edu>
2023-03-02 00:11:12 -0500

    ui creates for genre and theater

--- frontend/src/pages/Genres.js
+++ frontend/src/pages/Genres.js
@@ -1,43 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { Grid, _ } from "gridjs-react";
 import { MdEdit, MdDeleteForever } from "react-icons/md";
+import axios from "axios";
 
 export default function Genres() {
     return (
         <div>
             <h3>Genres</h3>
             <p>Create, Retrieve, Update or Delete a Genre</p>
-            <a href="./GenreNew" class="newPlus">
+            <Link to="./GenreNew" class="newPlus">
                 Add new genre
-            </a>
+            </Link>
             <Grid
-                data={[
-                    [1, "Documentary"],
-                    [2, "Kids"],
-                    [3, "Family"],
-                    [4, "Comedy"],
-                    [5, "Independent"],
-                    [6, "International"],
-                    [7, "Drama"],
-                    [8, "Musical"],
-                    [9, "Thriller"],
-                    [10, "Horror"],
-                    [11, "Sci-Fi"],
-                    [12, "Romance"],
-                    [13, "Animated"],
-                    [14, "Sports"],
-                    [15, "Action"],
-                    [16, "Cult Classic"],
-                    [17, "Adventure"],
-                    [18, "LGBTQ+"],
-                    [19, "Crime"],
-                    [20, "Mystery"],
-                    [21, "Fantasy"],
-                    [22, "Historical"],
-                ]}
                 columns={[
-                    { name: "Genre ID", sort: true },
-                    { name: "Genre Name", sort: true },
+                    { name: "Genre ID", id:"genre_id", sort: true },
+                    { name: "Genre Name", id:"genre_name", sort: true },
                     {
                         name: "Edit Item",
                         data: _(<MdEdit onClick={() => alert("clicked!")} />),
@@ -51,6 +29,16 @@ export default function Genres() {
                         ),
                     },
                 ]}
+
+                data={
+                    async () => {
+                    const genres = await axios.get(
+                        "http://localhost:3001/genres"
+                    );
+                    return genres.data.data;
+                }
+              }
+
                 search={true}
                 pagination={{ limit: 25 }}
             />
