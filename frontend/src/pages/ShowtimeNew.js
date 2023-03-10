b886e5f58d894eee445b407447b97ef381a42fb1
LoganC <alclary@posteo.net>
2023-03-09 23:26:48 -0600

    dropdown fetch working

--- frontend/src/pages/ShowtimeNew.js
+++ frontend/src/pages/ShowtimeNew.js
@@ -1,20 +1,59 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
-import { useHistory, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const API_URL = process.env.REACT_APP_API_URL;
 
 export const ShowtimeNew = () => {
     const navigate = useNavigate();
 
-    const [showtime_date_time, set_showtime_date_time] = useState("");
-    const [movie_id, set_movie_id] = useState("");
-    const [theatre_id, set_theater_id] = useState("");
+    const [movieList, setMovieList] = useState([]);
+    const [theaterList, setTheaterList] = useState([]);
 
-    const handleSubmit = (e) => {
+    useEffect(() => {
+        async function getMovieList() {
+            try {
+                const movies = await axios.get(API_URL + "/movies/titles");
+                setMovieList(movies.data.data);
+            } catch (error) {
+                console.error(error);
+                // TODO add user feedback of failure
+            }
+        }
+        async function getTheaterList() {
+            try {
+                const theaters = await axios.get(
+                    API_URL + "/theaters/locations"
+                );
+                setTheaterList(theaters.data.data);
+            } catch (error) {
+                console.error(error);
+                // TODO add user feedback of failure
+            }
+        }
+
+        getTheaterList();
+        getMovieList();
+    }, []);
+
+    const [showtime_date_time, setShowTimeDateTime] = useState("");
+    const [movie_id, setMovieId] = useState("");
+    const [theater_id, setTheaterId] = useState("");
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        // TODO implement POST request to API
+        try {
+            const res = await axios.post(API_URL + "/showtimes", {
+                showtime_date_time,
+                movie_id,
+                theater_id,
+            });
+            console.log(res);
+            // TODO replace with feedback of success and redirect to showtimes table
+        } catch (error) {
+            console.error(error);
+            // TODO add user feedback of failure
+        }
 
         navigate("/Showtime");
     };
@@ -26,22 +65,62 @@ export const ShowtimeNew = () => {
                 <form className="form">
                     <label>
                         Date & Time
-                        <input type="datetime-local" required />
+                        <input
+                            type="datetime-local"
+                            value={showtime_date_time}
+                            onChange={(e) => {
+                                setShowTimeDateTime(e.target.value);
+                            }}
+                            required
+                        />
                     </label>
                     <br />
-
                     <label>
                         Movie
-                        <input type="text" required />
+                        <select
+                            name="movie"
+                            value={movie_id}
+                            onChange={(e) => {
+                                setMovieId(e.target.value);
+                            }}
+                            required
+                        >
+                            <option disabled selected value="">
+                                -- select an option --
+                            </option>
+                            {movieList.map((movie, i) => {
+                                return (
+                                    <option key={i} value={movie.movie_id}>
+                                        {movie.movie_name}
+                                    </option>
+                                );
+                            })}
+                        </select>
                     </label>
                     <br />
-
                     <label>
                         Theater
-                        <input type="text" required />
+                        <select
+                            name="movie"
+                            value={theater_id}
+                            onChange={(e) => {
+                                setTheaterId(e.target.value);
+                            }}
+                            required
+                        >
+                            <option disabled selected value="">
+                                -- select an option --
+                            </option>
+                            {theaterList.map((theater, i) => {
+                                return (
+                                    <option key={i} value={theater.theater_id}>
+                                        {theater.theater_name}
+                                    </option>
+                                );
+                            })}
+                        </select>
                     </label>
                     <br />
-
                     <button
                         type="submit"
                         className="pure-button pure-button-primary"
@@ -51,7 +130,6 @@ export const ShowtimeNew = () => {
                     </button>
                 </form>
             </article>
-
             <a href="./Showtime">Return to all showtimes</a>
         </>
     );
