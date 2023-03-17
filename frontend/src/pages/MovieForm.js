415682b9f322b15343e2ee99c0b73db931e2635b
LoganC <alclary@posteo.net>
2023-03-16 21:40:37 -0500

    major refactor - same page bi-modal form

--- frontend/src/pages/MovieForm.js
+++ frontend/src/pages/MovieForm.js
@@ -1,137 +1,166 @@
 import React, { useState } from "react";
+import { toast } from "react-toastify";
 import axios from "axios";
-import { ReactWrapper } from "gridjs-react";
 
 const API_URL = process.env.REACT_APP_API_URL;
 
 export const MovieForm = (props) => {
-    console.log(props);
-
     const ratings = [
-        { rating: "R", label: "R" },
-        { rating: "NR", label: "NR" },
-        { rating: "NC-17", label: "NC-17" },
         { rating: "G", label: "G" },
         { rating: "PG", label: "PG" },
         { rating: "PG-13", label: "PG-13" },
+        { rating: "R", label: "R" },
+        { rating: "NR", label: "NR" },
+        { rating: "NC-17", label: "NC-17" },
     ];
 
+    // State definitions
+    //      If formType mode "edit", populate fields with record data to edit,
+    //      else form field empty (for new entry)
+    const [movieName, setMovieName] = useState(
+        props.formType === "edit" ? props.rowData.movie_name : ""
+    );
+    const [runtime, setRuntime] = useState(
+        props.formType === "edit" ? props.rowData.runtime_min : ""
+    );
+    const [rating, setRating] = useState(
+        props.formType === "edit" ? props.rowData.mpa_rating : ""
+    );
+    const [year, setYear] = useState(
+        props.formType === "edit" ? props.rowData.movie_year : ""
+    );
+
+    // Handle "new" record form submissions
     async function newSubmit() {
         try {
-            const res = await axios.post(API_URL + "/movies", {});
+            const res = await axios.post(API_URL + "/movies", {
+                movieName,
+                runtime,
+                rating,
+                year,
+            });
+            if (res === 200) {
+            }
             console.log(res);
-            // TODO replace with feedback of success and redirect to movies table
+            // Success toast notification
+            toast.success(`Record ID ${res.data.data.insertId} created.`);
+            // Reload entity table / grid.js component (for updates)
+            props.gridReload();
         } catch (error) {
+            toast.error(error.message);
             console.error(error);
-            // TODO add user feedback of failure
         }
+        props.resetForm();
     }
 
-    async function updateSubmit() {
+    // Handle "edit" record form submissions
+    async function editSubmit() {
         try {
             const res = await axios.put(
-                API_URL + `/movies/${props.row.movie_id}`,
+                API_URL + `/movies/${props.rowData.movie_id}`,
                 {
-                    movie_name,
-                    runtime_min,
-                    mpa_rating,
-                    movie_year,
+                    movieName,
+                    runtime,
+                    rating,
+                    year,
                 }
             );
-            if (res.status === 200) {
-                props.parentRerender();
+            if (res === 200) {
             }
-            // TODO replace with feedback of success and redirect to movies table
+            console.log(res);
+            // Success toast notification
+            toast.success(`Record ID updated.`);
+            // Reload entity table / grid.js component (for updates)
+            props.gridReload();
         } catch (error) {
+            toast.error(error.message);
             console.error(error);
-            // TODO add user feedback of failure
         }
-        props.showForm(false);
+        props.resetForm();
     }
 
-    // State definitions
-    const [movie_name, set_movie_name] = useState(
-        props.row.movie_name ? props.row.movie_name : ""
-    );
-    const [runtime_min, set_runtime_min] = useState(
-        props.row.runtime_min ? props.row.runtime_min : ""
-    );
-    const [mpa_rating, set_mpa_rating] = useState(
-        props.row.mpa_rating ? props.row.mpa_rating : ""
-    );
-    const [movie_year, set_movie_year] = useState(
-        props.row.movie_year ? props.row.movie_year : ""
-    );
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        if (props.row) {
-            updateSubmit();
+    // Handle submit of bi-modal form; submit action based on form mode
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        if (props.formType === "edit") {
+            editSubmit();
         } else {
             newSubmit();
         }
     };
 
-    const handleRatingChange = async (e) => {
-        set_mpa_rating(e.target.value);
-    };
-
     return (
-        <div>
-            {props.row ? <h3>Update movie</h3> : <h3>Add a new movie</h3>}
+        <div className="form_wrapper">
+            {/* Form title based on mode ("edit" or "new") */}
+            {props.formType === "edit" ? (
+                <h3>Update movie</h3>
+            ) : (
+                <h3>Add a new movie</h3>
+            )}
 
             <form
                 onSubmit={handleSubmit}
                 className="pure-form pure-form-stacked"
             >
-                <label>Movie name</label>
+                <label>Movie Name</label>
                 <input
                     type="text"
                     id="stacked-fname"
                     required
-                    value={movie_name}
-                    onChange={(e) => set_movie_name(e.target.value)}
+                    value={movieName}
+                    onChange={(event) => setMovieName(event.target.value)}
                 />
                 <label>Runtime (mins)</label>
                 <input
-                    // TODO DATA VALIDATION
-
                     type="number"
                     required
-                    value={runtime_min}
-                    onChange={(e) => set_runtime_min(e.target.value)}
+                    value={runtime}
+                    onChange={(event) => setRuntime(event.target.value)}
                 />
                 <label>MPA Rating</label>
-                <select onChange={handleRatingChange}>
-                    <option type="radio" name="mpa_rating" value={mpa_rating}>
-                        {mpa_rating}
+                <select
+                    name="rating"
+                    value={rating}
+                    onChange={(event) => setRating(event.target.value)}
+                    required
+                >
+                    <option disabled selected value="">
+                        -- select an option --
                     </option>
-                    {ratings.map((mpa_rating) => (
-                        <option value={mpa_rating.value}>
-                            {mpa_rating.label}
-                        </option>
-                    ))}
+                    {ratings.map((mpa_rating, i) => {
+                        return (
+                            <option key={i} value={mpa_rating.value}>
+                                {mpa_rating.label}
+                            </option>
+                        );
+                    })}
                 </select>
                 <label>Year</label>
                 <input
-                    // TODO DATA VALIDATION
-
                     type="number"
-                    value={movie_year}
-                    onChange={(e) => set_movie_year(e.target.value)}
+                    min="1878"
+                    max="2050"
+                    value={year}
+                    onChange={(event) => setYear(event.target.value)}
                 />
-                <button type="submit" class="pure-button pure-button-primary">
+                <button
+                    type="submit"
+                    className="pure-button pure-button-primary"
+                >
                     Submit
                 </button>
-                <button
-                    type="button"
-                    class="pure-button pure-button"
-                    onClick={() => {
-                        props.showForm(false);
-                    }}
-                >
-                    Cancel
-                </button>
+                {props.formType === "edit" ? (
+                    <button
+                        type="button"
+                        className="pure-button pure-button"
+                        // Cancel button resets form to cancel edit attempt
+                        onClick={() => {
+                            props.resetForm();
+                        }}
+                    >
+                        Cancel
+                    </button>
+                ) : undefined}
             </form>
         </div>
     );
