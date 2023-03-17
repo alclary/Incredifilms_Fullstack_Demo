cd5fd3887bc24826b4864d4a24ed7b3970fa2215
velazqje <velazqje@oregonstate.edu>
2023-03-16 20:34:32 -0400

    refactored theater to match customer format add update delete work

--- frontend/src/pages/TheaterForm.js
+++ frontend/src/pages/TheaterForm.js
@@ -1,102 +1,117 @@
 import React, { useState } from "react";
+import { toast } from "react-toastify";
 import axios from "axios";
-import { ReactWrapper } from "gridjs-react";
 
 const API_URL = process.env.REACT_APP_API_URL;
 
 export const TheaterForm = (props) => {
-    console.log(props);
+  // State definitions
+  //      If formType mode "edit", populate fields with record data to edit,
+  //      else form field empty (for new entry)
+  const [theater_name, set_theater_name] = useState(
+    props.formType === "edit" ? props.rowData.theater_name : "");
+const [no_of_seats, set_no_of_seats] = useState(
+    props.formType === "edit" ? props.rowData.no_of_seats : "");
 
-    async function newSubmit() {
-        try {
-            const res = await axios.post(API_URL + "/theaters", {
-                theater_name,
-                no_of_seats,
-            });
-            console.log(res);
-            // TODO replace with feedback of success and redirect to theaters table
-        } catch (error) {
-            console.error(error);
-            // TODO add user feedback of failure
-        }
+  // Handle "new" record form submissions
+  async function newSubmit() {
+    try {
+      const res = await axios.post(API_URL + "/theaters", {
+        theater_name,
+        no_of_seats,
+      });
+      if (res === 200) {
+      }
+      // Success toast notification
+      toast.success(`Record ID ${res.data.data.insertId} created.`);
+      // Reload entity table / grid.js component (for updates)
+      props.gridReload();
+    } catch (error) {
+      toast.error(error.message);
+      console.error(error);
     }
+    props.resetForm();
+  }
 
-    async function updateSubmit() {
-        try {
-            const res = await axios.put(
-                API_URL + `/theaters/${props.row.theater_id}`,
-                {
-                    theater_name,
-                    no_of_seats,
-                }
-            );
-            if (res.status === 200) {
-                props.parentRerender();
-            }
-            // TODO replace with feedback of success and redirect to theaters table
-        } catch (error) {
-            console.error(error);
-            // TODO add user feedback of failure
+  // Handle "edit" record form submissions
+  async function editSubmit() {
+    try {
+      const res = await axios.put(
+        API_URL + `/theaters/${props.rowData.theater_id}`,
+        {
+          theater_name,
+          no_of_seats,
         }
-        props.showForm(false);
+      );
+      if (res.status === 200) {
+      }
+      // Success toast notification
+      toast.success(`Record ID ${res.data.data.insertId} updated.`);
+      // Reload entity table / grid.js component (for updates)
+      props.gridReload();
+    } catch (error) {
+      toast.error(error.message);
+      console.error(error);
     }
+    props.resetForm();
+  }
 
-    // State definitions
-    const [theater_name, set_theater_name] = useState(
-        props.row.theater_name ? props.row.theater_name : ""
-    );
-    const [no_of_seats, set_no_of_seats] = useState(
-        props.row.no_of_seats ? props.row.no_of_seats : ""
-    );
+  // Handle submit of bi-modal form; submit action based on form mode
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (props.formType === "edit") {
+      editSubmit();
+    } else {
+      newSubmit();
+    }
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        if (props.row) {
-            updateSubmit();
-        } else {
-            newSubmit();
-        }
-    };
+  return (
+    <div>
+      {/* Form title based on mode ("edit" or "new") */}
 
-    return (
-        <div>
-            {props.row ? <h3>Update theater</h3> : <h3>Add a new theater</h3>}
+      {props.formType === "edit" ? (
+        <h3>Update a theater</h3>
+      ) : (
+        <h3>Add a new theater</h3>
+      )}
 
-            <form
-                onSubmit={handleSubmit}
-                className="pure-form pure-form-stacked"
-            >
-                <label>Theater name</label>
-                <input
-                    type="text"
-                    id="stacked-fname"
-                    required
-                    value={theater_name}
-                    onChange={(e) => set_theater_name(e.target.value)}
-                />
-                <label>Number of seats</label>
-                <input
-                    type="number"
-                    required
-                    value={no_of_seats}
-                    onChange={(e) => set_no_of_seats(e.target.value)}
-                />
+      <form onSubmit={handleSubmit} className="pure-form pure-form-stacked">
+        <label>Theater name</label>
+        <input
+          type="text"
+          id="stacked-fname"
+          required
+          value={theater_name}
+          onChange={(event) => set_theater_name(event.target.value)}
+        />
+        <label>Number of seats</label>
+        <input
+          type="number"
+          required
+          value={no_of_seats}
+          onChange={(event) => set_no_of_seats(event.target.value)}
+        />
 
-                <button type="submit" class="pure-button pure-button-primary">
-                    Submit
-                </button>
-                <button
-                    type="button"
-                    class="pure-button pure-button"
-                    onClick={() => {
-                        props.showForm(false);
-                    }}
-                >
-                    Cancel
-                </button>
-            </form>
-        </div>
-    );
+        <button type="submit" class="pure-button pure-button-primary">
+          Submit
+        </button>
+        {/* Cancel button only displayed for "edit" form modality */}
+        {props.formType === "edit" ? (
+          <button
+            type="button"
+            class="pure-button pure-button"
+            // Cancel button resets form to cancel edit attempt
+            onClick={() => {
+              props.resetForm();
+            }}
+          >
+            Cancel
+          </button>
+        ) : undefined}
+      </form>
+    </div>
+  );
 };
 
 export default TheaterForm;
