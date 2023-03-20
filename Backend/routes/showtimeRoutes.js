76f77a5404c88a8ce666b6d84ef727a3cb5bb253
LoganC <alclary@posteo.net>
2023-03-19 21:11:33 -0500

    expand standard showtime table to also provide id columns

--- Backend/routes/showtimeRoutes.js
+++ Backend/routes/showtimeRoutes.js
@@ -6,7 +6,7 @@ const db = require("../db.js");
 router.get("/", (req, res) => {
     console.log("GET request received.");
     db.query(
-        "SELECT Showtime.showtime_id AS showtime_id, Showtime.showtime_date_time AS date_time, Movie.movie_name AS movie_id, Theater.theater_name AS theater_id FROM Showtime JOIN Movie ON Showtime.movie_id = Movie.movie_id JOIN Theater ON Showtime.theater_id = Theater.theater_id ORDER BY showtime_id ASC;",
+        "SELECT Showtime.showtime_id AS showtime_id, Showtime.showtime_date_time AS date_time, Showtime.movie_id AS movie_id, Movie.movie_name AS movie_name, Showtime.theater_id AS theater_id, Theater.theater_name AS theater_name FROM Showtime JOIN Movie ON Showtime.movie_id = Movie.movie_id JOIN Theater ON Showtime.theater_id = Theater.theater_id ORDER BY showtime_id ASC;",
         (err, data, fields) => {
             if (err) {
                 console.error(err);
