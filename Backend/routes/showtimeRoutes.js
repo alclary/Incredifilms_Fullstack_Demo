5f9541ea7ac79f2675efdf1499768549d801ee37
LoganC <alclary@posteo.net>
2023-03-20 19:46:48 -0500

    show all showtime rows

--- Backend/routes/showtimeRoutes.js
+++ Backend/routes/showtimeRoutes.js
@@ -11,7 +11,7 @@ router.get("/", (req, res) => {
     //     moment.utc(req.query.enddate).format("YYYY/MM/DD HH:mm:ss")
     // );
     db.query(
-        "SELECT Showtime.showtime_id AS showtime_id, Showtime.showtime_date_time AS date_time, Showtime.movie_id AS movie_id, Movie.movie_name AS movie_name, Showtime.theater_id AS theater_id, Theater.theater_name AS theater_name FROM Showtime JOIN Movie ON Showtime.movie_id = Movie.movie_id JOIN Theater ON Showtime.theater_id = Theater.theater_id WHERE Showtime.showtime_date_time BETWEEN ? AND ? ORDER BY showtime_id ASC;",
+        "SELECT Showtime.showtime_id AS showtime_id, Showtime.showtime_date_time AS date_time, Showtime.movie_id AS movie_id, Movie.movie_name AS movie_name, Showtime.theater_id AS theater_id, Theater.theater_name AS theater_name FROM Showtime LEFT JOIN Movie ON Showtime.movie_id = Movie.movie_id LEFT JOIN Theater ON Showtime.theater_id = Theater.theater_id WHERE Showtime.showtime_date_time BETWEEN ? AND ? ORDER BY showtime_id ASC;",
         [
             moment.utc(req.query.startdate).format("YYYY/MM/DD HH:mm:ss"),
             moment.utc(req.query.enddate).format("YYYY/MM/DD HH:mm:ss"),
