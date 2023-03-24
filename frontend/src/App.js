1b08cc4097a286c626fe69a60b65576eaa99f348
LoganC <alclary@posteo.net>
2023-03-24 12:45:22 -0500

    better static site support (github pages)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
 import Sidebar from "./components/Sidebar";
