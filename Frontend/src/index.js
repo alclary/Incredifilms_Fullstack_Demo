94830ee65dfe1365451281979e461eed2737a0b3
LoganC <alclary@posteo.net>
2023-03-25 15:58:06 -0500

    change css custom priority; remove !important tags

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { ToastContainer } from "react-toastify";
-import "./css/custom.css";
-import "./css/grid-min.css";
 import "./css/normalize.css";
+import "./css/grid-min.css";
 import "./css/pure-min.css";
 import "react-toastify/dist/ReactToastify.min.css";
+import "./css/custom.css";
 import App from "./App";
 
 createRoot(document.getElementById("root")).render(
